4f1bdff44e9ed9ee49e65fdf0d119859a6de6167
Zainal <zainal.arifin@ilcs.co.id>
2022-12-14 21:31:48 +0700

    node modules

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const { default: axios } = require("axios");
+var express = require("express");
+var router = express.Router();
+var xml2js = require("xml2js");
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  let { data } = await axios({
+    method: "get",
+    url: "https://api.flickr.com/services/feeds/photos_public.gne",
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+
+  // convert XML to JSON
+  let result = xml2js.parseString(data, { mergeAttrs: true }, (err, result) => {
+    if (err) {
+      throw err;
+    }
+
+    const json = result;
+
+    res.json({ data: json });
+  });
+});
+
+module.exports = router;
